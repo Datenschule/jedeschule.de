5cc484b5cdcdce1bee0136dd10fab55373a37e40
Knut Perseke <knut.perseke@gmail.com>
2016-12-31 20:12:35 +0100

    update schoolkids controller

--- assets/js/app/controller/schookindsController.js
+++ assets/js/app/controller/schookindsController.js
@@ -1,19 +1,23 @@
 app.controller('SchoolkindsController', function ($scope, $http, schools) {
-    $scope.data = {
-        labels: [
-            "Schulkindergärten",
-            "Grundschulen",
-            "Schulartunabhängige Orientierungsstufe",
-            "Hauptschulen",
-            "Realschulen",
-            "Gymnasien",
-            "Integrierte Gesamtschulen",
-            "Freie Waldorfschulen",
-            "Förderschulen",
-            "Abendrealschulen",
-            "Abendgymnasien",
-            "Kollegs"
-        ],
-        series: [430,2460,4,1291,445,411,3,34,539,50,18,8]
+    $scope.data = [430,2460,4,1291,445,411,3,34,539,50,18,8];
+
+    $scope.test = function() {
+        $scope.data = [43,2460,4,1291,445,411,3,34,539,50,18,8];
     };
+
+    $scope.categories = ["Schulkindergärten",
+        "Grundschulen",
+        "Schulartunabhängige Orientierungsstufe",
+        "Hauptschulen",
+        "Realschulen",
+        "Gymnasien",
+        "Integrierte Gesamtschulen",
+        "Freie Waldorfschulen",
+        "Förderschulen",
+        "Abendrealschulen",
+        "Abendgymnasien",
+        "Kollegs"];
+
+    $scope.colors = ['#0000b4', '#0082ca', '#0094ff', '#0d4bcf', '#0066AE', '#074285', '#00187B', '#285964', '#405F83', '#416545', '#4D7069', '#6E9985', '#7EBC89', '#0283AF', '#79BCBF', '#99C19E'];
+
 });
