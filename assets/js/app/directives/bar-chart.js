f8f11cb88a4247ea6addc5be5006560d478b339e
Knut Perseke <knut.perseke@gmail.com>
2016-12-31 20:12:10 +0100

    update bar chart

--- assets/js/app/directives/bar-chart.js
+++ assets/js/app/directives/bar-chart.js
@@ -6,135 +6,182 @@ app.directive('barchart', function() {
 
     return {
         restrict: 'E',
-        // scope: {
-        //     data: '='
-        // },
-        template:'<div class="ds-barchart"></div>',
+        scope: {
+            data: '=',
+            caption: '=',
+            colors: '='
+        },
+        template: '<div class="ds-barchart"></div>',
         replace: true,
-        link: function(scope, element, attrs) {
+        link: function (scope, element, attrs) {
 
-            var categories= ["Schulkindergärten",
-                "Grundschulen",
-                "Schulartunabhängige Orientierungsstufe",
-                "Hauptschulen",
-                "Realschulen",
-                "Gymnasien",
-                "Integrierte Gesamtschulen",
-                "Freie Waldorfschulen",
-                "Förderschulen",
-                "Abendrealschulen",
-                "Abendgymnasien",
-                "Kollegs"];
+            var grid = d3.range(25).map(function (i) {
+                return {'x1': 0, 'y1': 0, 'x2': 0, 'y2': 480};
+            });
 
             var canvas = d3.select(element[0])
                 .append("svg")
                 .attr("width", width)
                 .attr("height", height + margin + 100);
 
-            var dollars = [430,2460,4,1291,445,411,3,34,539,50,18,8];
-
-            var colors = ['#0000b4','#0082ca','#0094ff','#0d4bcf','#0066AE','#074285','#00187B','#285964','#405F83','#416545','#4D7069','#6E9985','#7EBC89','#0283AF','#79BCBF','#99C19E'];
-
-            var grid = d3.range(25).map(function(i){
-                return {'x1':0,'y1':0,'x2':0,'y2':480};
+            var tickVals = grid.map(function (d, i) {
+                if (i > 0) {
+                    return i * 10;
+                }
+                else if (i === 0) {
+                    return "100";
+                }
             });
-
-            var tickVals = grid.map(function(d,i){
-                if(i > 0){ return i * 10; }
-                else if(i === 0){ return "100";}
-            });
-
-            var xscale = d3.scaleLinear()
-                .domain([0,2800])
-                .range([0,622]);
-
-            var yscale = d3.scaleLinear()
-                .domain([0,categories.length])
-                .range([0,480]);
-
-            var colorScale = d3.scaleQuantize()
-                .domain([0,categories.length])
-                .range(colors);
 
             var chart_background = canvas.append('g')
                 .attr("transform", "translate(0,0)")
-                .attr('id','bars-background')
-                .selectAll('rect-background')
-                .data(dollars)
-                .enter()
-                .append('rect')
-                .attr('class', 'rect-background')
-                .attr('height',26)
-                .attr('x', 0)
-                .attr('y', function(d,i){ return yscale(i); })
-                .style('fill', '#e9eaee')
-                .attr('width', xscale(2800));
-
+                .attr('id', 'bars-background');
             var chart_description = canvas.append('g')
                 .attr("transform", "translate(0,0)")
-                .attr('id','bars-background')
-                .selectAll('rect-background')
-                .data(dollars)
-                .enter()
-                // .append('a')
-                // .attr({"xlink:href": "#"})
-                .append('text')
-                .attr('x', xscale(2800) + 10)
-                .attr('y',function(d,i){ return yscale(i) + 19 })
-                .text(function(d,i){ return categories[i]; })
-                .style('fill', '#112233')
-                .style('font-size','14px');
-
+                .attr('id', 'bars-background');
             var chart_background_sep = canvas.append('g')
                 .attr("transform", "translate(0,0)")
-                .attr('id','bars-background')
-                .selectAll('rect-background')
-                .data(dollars)
-                .enter()
-                .append('rect')
-                .attr('height', 26)
-                .attr('class', 'bars-backround-sep')
-                .attr('x', xscale(2800) - 2)
-                .attr('y', function(d,i){ return yscale(i); })
-                .style('fill', '#9aa9b8')
-                .attr('width', 2)
-                .text('test');
-
+                .attr('id', 'bars-background');
             var chart = canvas.append('g')
                 .attr("transform", "translate(0,0)")
-                .attr('id','bars')
-                .selectAll('.rect-val')
-                .data(dollars)
-                .enter()
-                .append('rect')
-                .attr('height',26)
-                .attr('class', 'rect-value')
-                .attr('x', 0)
-                .attr('y', function(d,i){ return yscale(i); })
-                .style('fill', function(d,i) {
-                    return colors[i]
-                })
-                .attr('width',function(d){ return xscale(d); });
+                .attr('id', 'bars');
+
+            scope.$watch('caption', function (newVal, oldVal) {
+                scope.caption = newVal;
+                updateChart();
+            });
+
+            scope.$watch('data', function (newVal, oldVal) {
+                //newVal = newVal || [];
+                scope.data = newVal;
+                updateChart();
+            });
+
+            scope.$watch('colors', function (newVal, oldVal) {
+                //newVal = newVal || [];
+                scope.colors = newVal;
+                updateChart();
+            });
+
+            function updateChart() {
+                console.log(scope.caption);
+                console.log(scope.data);
+                console.log(scope);
+                var xscale = d3.scaleLinear()
+                    .domain([0, 2800])
+                    .range([0, 622]);
+
+                var yscale = d3.scaleLinear()
+                    .domain([0, scope.caption.length])
+                    .range([0, 480]);
+
+                var colorScale = d3.scaleQuantize()
+                    .domain([0, scope.caption.length])
+                    .range(scope.colors);
+
+                chart_background
+                    .selectAll('rect-background')
+                    .data(scope.data)
+                    .enter()
+                    .append('rect')
+                    .attr('class', 'rect-background')
+                    .attr('height', 26)
+                    .attr('x', 0)
+                    .attr('y', function (d, i) {
+                        return yscale(i);
+                    })
+                    .style('fill', '#e9eaee')
+                    .attr('width', xscale(2800));
+
+               chart_description
+                    .selectAll('rect-background')
+                    .data(scope.data)
+                    .enter()
+                    // .append('a')
+                    // .attr({"xlink:href": "#"})
+                    .append('text')
+                    .attr('x', xscale(2800) + 10)
+                    .attr('y', function (d, i) {
+                        return yscale(i) + 19
+                    })
+                    .text(function (d, i) {
+                        return scope.caption[i];
+                    })
+                    .style('fill', '#112233')
+                    .style('font-size', '14px');
+
+                chart_background_sep
+                    .selectAll('rect-background')
+                    .data(scope.data)
+                    .enter()
+                    .append('rect')
+                    .attr('height', 26)
+                    .attr('class', 'bars-backround-sep')
+                    .attr('x', xscale(2800) - 2)
+                    .attr('y', function (d, i) {
+                        return yscale(i);
+                    })
+                    .style('fill', '#9aa9b8')
+                    .attr('width', 2)
+                    .text('test');
+
+                var chartGroup = chart
+                    .selectAll('.bar')
+                    .data(scope.data)
+                    .enter()
+                    .append('g')
+                    .attr('class', 'bar');
+
+                chartGroup.append('rect')
+                    .attr('height', 26)
+                    .attr('class', 'rect-value')
+                    .attr('x', 0)
+                    .attr('y', function (d, i) {
+                        return yscale(i);
+                    })
+                    .style('fill', function (d, i) {
+                        return scope.colors[i]
+                    })
+                    .attr('width', function (d) {
+                        return xscale(d);
+                    });
+
+                chartGroup.append('text')
+                    .attr('class', 'bar-text')
+                    .attr('x', function (d) {
+                        return xscale(d) + 10.5
+                    })
+                    .attr('y', function (d, i) {
+                        return yscale(i) + 19
+                    })
+                    .text(function (d) {
+                        return d;
+                    })
+                    .style('fill', '#000')
+                    .style('font-size', '14px');
 
 
-            var transit = canvas.select("svg").selectAll(".rect-value")
-                .data(dollars)
-                .transition()
-                .duration(1000)
-                .attr("width", function(d) {return xscale(d); });
-
-            var transitext = canvas.select('#bars')
-                .selectAll('text')
-                .data(dollars)
-                .enter()
-                .append('text')
-                .attr('x',function(d) {
-                    return xscale(d) + 10.5})
-                .attr('y',function(d,i){ return yscale(i) + 19 })
-                .text(function(d){
-                    return d; })
-                .style('fill', '#000')
-                .style('font-size','14px')
+                var transit = canvas.selectAll(".rect-value")
+                    .data(scope.data)
+                    .transition()
+                    .duration(1000)
+                    .attr("width", function (d) {
+                        return xscale(d);
+                    });
+                var transittext = canvas.selectAll(".bar-text")
+                    .data(scope.data)
+                    .transition()
+                    .duration(1000)
+                    .attr('x', function (d) {
+                        return xscale(d) + 10.5
+                    })
+                    .attr('y', function (d, i) {
+                        return yscale(i) + 19
+                    })
+                    .text(function (d) {
+                        return d;
+                    });
+            }
         }
     }
 });
