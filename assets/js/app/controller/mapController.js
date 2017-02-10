b4868740192ce1aeca437da55681211f697d5400
Knut Hühne <knut@k-nut.eu>
2017-02-10 17:42:39 +0100

    speed up rendering of map

--- assets/js/app/controller/mapController.js
+++ assets/js/app/controller/mapController.js
@@ -8,23 +8,37 @@ app.controller('MapController', function ($scope, $http, schools) {
     $scope.schoolProfileFilter = false;
 
     var filter_keys = ['school_type', 'legal_status'];
-    var layer = undefined;
-    var base_url = "https://lab.okfn.de/jedeschule/data";
+    var layer;
+    var allSchools;
 
     schools.overview(function(err, schools) {
         console.log('loaded schools');
-        initMap(schools);
-        initFilters(schools);
-        $scope.all_schools = schools.map(function(school) { school.filtered = false; return school;});
-        console.log('finished adding markers');
+
+        console.time("mapSchools");
+        allSchools = schools;
+        console.timeEnd("mapSchools");
+
+        console.time("display");
         display();
+        console.timeEnd("display");
+
+        console.time("initFilters");
+        initFilters(schools);
+        console.timeEnd("initFilters");
+
+        console.log('finished adding markers');
     });
 
     function initFilters(schools) {
         filter_keys.forEach(function(key) {
-            var entries = schools.map(function(school) { return school[key] });
-            var unique_entries = entries.filter(function(v, i, a) { return a.indexOf(v) === i });
-            var filterEntries = unique_entries.reduce(function(prev,entry) { if (entry) prev.push({name: entry}); return prev }, []);
+            var filterEntries = _.chain(schools)
+                                 .map(key)
+                                 .uniq()
+                                 .filter(function (x){ return x })
+                                 .map(function(x){
+                                     return {name: x};
+                                 })
+                                 .value();
             $scope.filter[key] = filterEntries;
         })
     }
@@ -36,26 +50,6 @@ app.controller('MapController', function ($scope, $http, schools) {
     L.tileLayer('https://api.mapbox.com/styles/v1/okfde/ciwxo7szj00052pnx7xgwdl1d/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoib2tmZGUiLCJhIjoiY2lpOHhvMnNhMDAyNnZla280ZWhmMm96NyJ9.IvGz74dvvukg19B4Npsm1g', {
         attribution: '&copy; <a href="https://www.mapbox.com">Map Box</a> contributors'
     }).addTo(map);
-
-    function initMap(schools) {
-        var markers = L.markerClusterGroup({
-            showCoverageOnHover: false,
-            zoomToBoundsOnClick: false,
-            singleMarkerMode: true});
-        markers.on('click', onMarkerClick);
-
-        var theMarkers = schools.filter(function(school){
-            return school.lat && school.lon;
-        }).map(function(school){
-            var marker = L.marker([school.lat, school.lon]);
-            marker.school = school;
-            return marker
-        });
-        markers.addLayers(theMarkers);
-        layer = markers;
-        map.addLayer(markers);
-        //display();
-    }
 
     function onMarkerClick(marker) {
         $scope.singleSchool = true;
@@ -69,22 +63,33 @@ app.controller('MapController', function ($scope, $http, schools) {
         $scope.$apply();
     }
 
-    var display = function() {
-        if (!$scope.all_schools) {
-            return;
-        }
-        var filtered = $scope.all_schools.map(function(school) { school.displayed = true; return school;});
-        //apply filters
-        for (var filter in $scope.selected) {
-            var entries = $scope.selected[filter].map(function (x){ return x.name });
-            filtered.forEach(function (school) {
-                if (school.displayed) {
-                    if (entries.length && entries.indexOf(school[filter]) < 0){
-                        school.displayed = false;
+    function onClusterClick(cluster){
+        $scope.singleSchool = false;
+        // cluster.layer is actually a cluster
+        $scope.schools = _.countBy(cluster.layer.getAllChildMarkers(), "school.school_type");
+        $scope.infoboxHidden = false;
+        $scope.$apply();
+    }
+
+    $scope.closeSlider = function(){
+        $scope.infoboxHidden = true;
+    };
+
+    function filterSchools(schools){
+        var filtered = schools.map(function(school) { school.displayed = true; return school;});
+
+        _.forEach($scope.selected, function(filter){
+            var entries = filter.map(function (x){ return x.name });
+            if (entries.length) {
+                filtered.forEach(function (school) {
+                    if (school.displayed) {
+                        if (entries.indexOf(school[filter]) < 0) {
+                            school.displayed = false;
+                        }
                     }
-                }
-            });
-        }
+                })
+            }
+        });
         if ($scope.schoolProfileFilter){
             filtered.forEach(function(school){
                 if (school.displayed && !school.profile){
@@ -92,46 +97,51 @@ app.controller('MapController', function ($scope, $http, schools) {
                 }
             })
         }
-        $scope.all_schools = filtered;
+
+        return filtered;
+    }
+
+    var display = function() {
+        if (!allSchools) {
+            return;
+        }
+        var filtered = filterSchools(allSchools);
+
         if (layer) {
             map.removeLayer(layer);
         }
 
         var markers = L.markerClusterGroup({
+            chunkedLoading: true,
             showCoverageOnHover: false,
             zoomToBoundsOnClick: false,
             singleMarkerMode: true});
         markers.on('click', onMarkerClick);
-        markers.on('clusterclick', function (a) {
-            $scope.singleSchool = false;
-            // a.layer is actually a cluster
-            $scope.schools = _.countBy(a.layer.getAllChildMarkers(), "school.school_type");
-            $scope.infoboxHidden = false;
-            $scope.$apply();
-        });
+        markers.on('clusterclick', onClusterClick);
 
-
-        var filteredMarkers = $scope.all_schools
+        console.time("making markers")
+        var filteredMarkers = filtered
             .filter(function(school){
                 return school.lat && school.lon && school.displayed;
             })
             .map(function(school) {
-                var marker = L.marker([school.lat, school.lon]);
+                var marker = L.marker(L.latLng(school.lat, school.lon));
                 marker.school = school;
                 return marker;
             });
+        console.timeEnd("making markers");
+
+        console.time("addLayers");
         markers.addLayers(filteredMarkers);
+        console.timeEnd("addLayers");
+
         layer = markers;
+        console.time('addLayer');
         map.addLayer(layer);
+        console.timeEnd('addLayer')
+
     };
 
-    $scope.$watch('schoolProfileFilter',display);
-
-    filter_keys.forEach(function(key) {
-        $scope.selected[key] = [];
-        $scope.$watch('selected.' + key, function(o) {
-            display();
-        })
-    })
-
+    $scope.$watchGroup(['schoolProfileFilter', 'fullTimeSchoolFilter'], display);
+    $scope.$watchCollection('selected', display);
 });
