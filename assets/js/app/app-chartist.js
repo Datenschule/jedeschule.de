bc6ac7623be94f0c6de8c553e2de74cc0eba9f76
Knut Perseke <knut.perseke@gmail.com>
2017-03-14 21:26:13 +0100

    bind html for leitbilder

--- assets/js/app/app-chartist.js
+++ assets/js/app/app-chartist.js
@@ -1,4 +1,4 @@
-var app = angular.module('App', ['angular-chartist']);
+var app = angular.module('App', ['angular-chartist', 'ngSanitize']);
 app.config(['$compileProvider', function ($compileProvider) {
-    $compileProvider.debugInfoEnabled(false);
+    $compileProvider.debugInfoEnabled(true);
 }]);
